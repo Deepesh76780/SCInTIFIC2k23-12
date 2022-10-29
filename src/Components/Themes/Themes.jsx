a484c2102bb7238ce67aeae936db57a9b7b677a0
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-10-29 16:07:38 +0530

    css remaining work

--- src/Components/Themes/Themes.jsx
+++ src/Components/Themes/Themes.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export const Themes = () => {
-    return (
-        <div className='flex flex-col justify-center items-center'>
-            <div className=' text-5xl font-extrabold'>
-                Themes
-            </div>
-            <div className='text-transparent text-3xl font-bold bg-clip-text bg-gradient-to-r from-[#FF8008] to-[#FFC837] my-10'>
-            Classes 1 to 4
-            </div>
-            <div className='h-[200px]'>
-
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center Bag">
+      <div className=" text-5xl font-extrabold">Themes</div>
+      <div className="text-transparent text-3xl font-bold bg-clip-text bg-gradient-to-r from-[#FF8008] to-[#FFC837] my-10">
+        Classes 1 to 4
+      </div>
+      <div className="h-[200px]"></div>
+    </div>
+  );
+};
