c24154d44d4e3e14bb981a2b01c83305800f8545
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-10-29 14:37:09 +0530

    Navbar css Work

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,75 @@
-import React from 'react'
+import React from "react";
+import { Fragment } from "react";
+import "./Navbar.css";
+import { Spin as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 export const Navbar = () => {
-    return (
-        <div className='flex flex-row justify-between items-center p-4 text-[#272727]'>
-            <div className='flex flex-row justify-start items-center space-x-2 cursor-pointer'>
-                <div>
-                    <img src="./assets/logo.png" className='h-[64px] w-[64px]' alt="" />
-                </div>
-                <div className='flex flex-col uppercase justify-start items-start font-semibold text-xl tracking-wide -space-y-2.5'>
-                    <div>
-                        DR SPM IIIT
-                    </div>
-                    <div>
-                        NAYA RAIPUR
-                    </div>
-                </div>
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <Fragment>
+      <div className="flex flex-row justify-between items-center p-4 text-[#272727] okBoss">
+        <div className="flex flex-row justify-start items-center space-x-2 cursor-pointer Logo">
+          <div className="flex flex-row justify-start items-center space-x-2 cursor-pointer">
+            <div>
+              <img
+                src="./assets/logo.png"
+                className="h-[64px] w-[64px] NavbarLogo"
+                alt=""
+              />
             </div>
-            <div className='flex flex-row justify-center items-center space-x-8'>
-                <div className='font-medium text-lg cursor-pointer'>About</div>
-                <div className='font-medium text-lg cursor-pointer'>Themes</div>
-                <div className='font-medium text-lg cursor-pointer'>Schedule</div>
-                <div className='font-medium text-lg cursor-pointer'>How to participate</div>
-                <div className='font-medium text-lg cursor-pointer'>Contact</div>
+            <div className="flex flex-col uppercase justify-start items-start font-semibold text-xl tracking-wide -space-y-2.5 NavbarTitle">
+              <div>DR SPM IIIT</div>
+              <div>NAYA RAIPUR</div>
             </div>
-            <div className='text-[#5172E7] w-[180px] h-[48px] rounded-full border drop-shadow-lg flex justify-center items-center bg-white font-semibold text-opacity-80 cursor-pointer hover:drop-shadow-xl transition duration-100'>
-                Submit Your Idea
-            </div>
+          </div>
+          <div className="Hamburger">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </div>
         </div>
-    )
-}
+
+        <div
+          className={
+            "flex flex-row justify-center items-center space-x-8 NavbarDisplay NavbarAbout "
+          }
+        >
+          <div className="font-medium text-lg cursor-pointer Adjust">About</div>
+          <div className="font-medium text-lg cursor-pointer">Themes</div>
+          <div className="font-medium text-lg cursor-pointer">Schedule</div>
+          <div className="font-medium text-lg cursor-pointer">
+            How to participate
+          </div>
+          <div className="font-medium text-lg cursor-pointer">Contact</div>
+        </div>
+
+        <div className="text-[#5172E7] w-[180px] h-[48px] rounded-full border drop-shadow-lg flex justify-center items-center bg-white font-semibold text-opacity-80 cursor-pointer hover:drop-shadow-xl transition duration-100 NavbarSubmit NavbarDisplay">
+          Submit Your Idea
+        </div>
+        {isOpen ? (
+          <div
+            className={
+              !isOpen
+                ? ""
+                : "NAV_ABOUT flex flex-row justify-center items-center  NavbarDisplay "
+            }
+          >
+            <div className="font-medium text-lg cursor-pointer AboutFix">
+              About
+            </div>
+            <div className="font-medium text-lg cursor-pointer ">Themes</div>
+            <div className="font-medium text-lg cursor-pointer">Schedule</div>
+            <div className="font-medium text-lg cursor-pointer">
+              How to participate
+            </div>
+            <div className="font-medium text-lg cursor-pointer">Contact</div>
+            <div className="text-[#5172E7]  h-[30px] rounded-full border drop-shadow-lg  bg-white font-semibold text-opacity-80 cursor-pointer hover:drop-shadow-xl transition duration-100 NavbarSubmit SubmitIdea">
+              Submit Your Idea
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </Fragment>
+  );
+};
